f73545fa94de0a4c20ffd9e940af843fd083d8f7
Zach <32909572+Rudesind@users.noreply.github.com>
2018-11-09 14:48:04 -0800

    Clean-up
    Cleaned up the files and added my signature.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# PowerShell-Run-File
-This script runs an EXE or any other provided file with logging.
+# Powershell-Run-File
+
+---
+
+`Install_Package.ps1` launches the provided file with arguments and returns the error code.
+
+This script requires the use of `LOG.psm1`.
+
+`Start_EXE.ps` starts the exe in the current directory. Other exes in the directory will cause the script to error.
+
+## Syntax
+
+```powershell
+Install_Package.ps1 <file_path> <file> <arguments>
+```
+
+```powershell
+Start_EXE.ps1
+```
+
+## Error Codes
+
+### Install_Package.ps1
+
+(-1): The script could not be initialized.
+MODULE_LOAD_FAILED (4000): Could not load the needed modules.
+INITIALIZATION_FAILED (4002): Could not initialize needed variables.
+LOG_LOAD_FAILED (4003): Failed to initialize the log file.
+INSTALL_FAILED (4004): Failed to run\install the provided file.
+FAILED_TO_EXIT (4010): Failed to exit properly.
+
+### Start_EXE.ps1
+
+INITIALIZATION_FAILED (4000): Error occurred when initializing the variables.
+LOG_LOAD_FAILED (4001): Error initializing the log file.
+FAILED_TO_START_PROCESS (4003): Error starting exe, no exe, or too many exes
+FAILED_TO_EXIT (4004): Could not properly exit the script.
+
+### References
+
+---
+
+[No Column Headings](https://stackoverflow.com/questions/1408042/output-data-with-no-column-headings-using-powershell)
+[File Search](https://stackoverflow.com/questions/8677628/recursive-file-search-using-powershell)
---
+++ Start EXE/Start_EXE.ps1
@@ -0,0 +1,131 @@
+
+#-----------------------------------#
+# Script : Start_EXE.ps1
+# Updated: 11/9/2018
+# Author : Zach Nybo
+# Version: 1.0
+#
+# Summary:
+# This script starts a single exe in
+# the same directory.
+#-----------------------------------#
+
+#--------------------#
+# Script Information #
+#--------------------#
+
+New-Variable SCRIPT -option Constant -value "Start_EXE.ps1"
+New-Variable AUTHOR -option Constant -value "Zach Nybo"
+New-Variable UPDATED -option Constant -value "11/9/2018"
+New-Variable VERSION -option Constant -value "1.0"
+
+#-------------#
+# Error Codes #
+#-------------#
+
+New-Variable INITIALIZATION_FAILED -option Constant -value 4000
+New-Variable LOG_LOAD_FAILED -option Constant -value 4001
+New-Variable FAILED_TO_START_PROCESS -option Constant -value 4003
+New-Variable FAILED_TO_EXIT -option Constant -value 4004
+
+#--------------#
+# Log Function #
+#--------------#
+
+# Idea taken from:
+# https://stackoverflow.com/questions/7834656/create-log-file-in-powershell
+# The below function is used to write to a log file
+Function LogWrite {
+
+    # Parameters for the function
+    # $logsting is the string written to the file
+    Param ([string]$logstring)
+
+    # The current time stamp for the log entry. Updated with every write
+    $time = Get-Date -Format g
+
+    # Write the contents to a log file at the specified location
+    Add-content $logFile -value "$time $logstring" -ErrorAction Stop
+
+}
+
+#----------------------#
+# Initialize Variables #
+#----------------------#
+
+try {
+
+    # Friendly error message
+    $errorMsg = ""
+
+    # Time stamp is in ISO 8601 standard
+    $timestamp = Get-Date -format "yyyyMMddTHHmmss"
+
+    # Log file directory
+    $logDir = "C:\URM"
+
+    # Log file path
+    $logFile = "$logDir\Start_EXE_$timestamp.txt"
+
+} catch {
+    $errorMsg = "Error, could not Initialize script: " + $Error[0]
+    exit $INITIALIZATION_FAILED
+}
+
+#---------------------#
+# Initialize Log file #
+#---------------------#
+
+try {
+
+    LogWrite "---"
+    LogWrite $SCRIPT
+    LogWrite $UPDATED
+    LogWrite $AUTHOR
+    LogWrite $VERSION
+    LogWrite "---"
+
+} catch {
+    $errorMsg = "Error, could not load log file: " + $Error[0]
+    exit $LOG_LOAD_FAILED
+}
+
+#-----------#
+# Start EXE #
+#-----------#
+
+try {
+
+    LogWrite "Getting file name..."
+    $EXE = Get-ChildItem -Path $PWD -Filter *.exe | Foreach {$_.Name}
+
+    LogWrite "Done"
+
+    LogWrite "Starting $EXE..."
+    Start-Process $EXE
+
+    LogWrite "Done"
+
+} catch {
+    $errorMsg = "Error, could not start or locate EXE: " + $Error[0]
+    LogWrite $errorMsg
+    LogWrite "Exiting with error code: $FAILED_TO_START_PROCESS"
+    exit $FAILED_TO_START_PROCESS
+}
+
+#------#
+# Exit #
+#------#
+
+try {
+
+    # Exit
+    $code = "0"
+    LogWrite "Exiting script with error code: $code"
+
+} catch {
+    $errorMsg = "Error, code not exit: " + $Error[0]
+    LogWrite $errorMsg
+    LogWrite "Exiting with error code: $FAILED_TO_EXIT"
+    exit $FAILED_TO_EXIT
+}
